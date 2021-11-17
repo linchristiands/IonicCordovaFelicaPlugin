c02f2dbb4bac24f9a5068a5f6ce7fad083427606
Christian Lin <linchristiands@gmail.com>
2021-11-17 17:29:01 +0900

    added plugin for NFC android kotlin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { FelicaPlugin } from '@awesome-cordova-plugins/felica-plugin/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    FelicaPlugin,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,77 @@
 import { Component } from '@angular/core';
+import { NgZone } from '@angular/core';
+import { FelicaPlugin } from '@awesome-cordova-plugins/felica-plugin/ngx';
 
+declare var window: any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public idm = '';
+  public pmm = '';
+  public waonno = '';
+  public enabled = true;
+  constructor(private zone: NgZone, private felica: FelicaPlugin) {}
 
-  constructor() {}
+  public async readNFC() {
+    this.enabled = false;
+    this.idm = '';
+    this.pmm = '';
+    this.waonno = '';
 
+    var success = (message) => {
+      //alert(message);
+      console.log(message.idm);
+      console.log(message.pmm);
+      console.log(message.waonno);
+      this.zone.run(() => {
+        this.idm = message.idm;
+        this.pmm = message.pmm;
+        this.waonno = message.waonno;
+      });
+    };
+
+    var failure = function (error) {
+      console.log(error);
+      //alert(error);
+    };
+    //window.cordova.plugins.FelicaPlugin.startNfc('', success, failure);
+    this.felica
+      .startNfc('')
+      .then((res) => {
+        this.zone.run(() => {
+          this.idm = res.idm;
+          this.pmm = res.pmm;
+          this.waonno = res.waonno;
+        });
+      })
+      .catch((e) => {
+        //console.log(e);
+      });
+    //let res = await this.felica.startNfc();
+    //console.log(res);
+  }
+  /* public async Hello() {
+    let hello = await this.felica.Hello("Testetststs");
+    let coolmethod = await this.felica.coolMethod();
+    let nativeToast = await this.felica.nativeToast();
+    console.log(hello);
+    console.log(coolmethod);
+    console.log(nativeToast);
+  } */
+  public async stopNFC() {
+    this.enabled = true;
+    var success = function (message) {
+      console.log('closed NFC');
+    };
+
+    var failure = function (error) {
+      console.log(error);
+      //alert(error);
+    };
+    //window.cordova.plugins.FelicaPlugin.stopNfc('', success, failure);
+    await this.felica.stopNfc();
+  }
 }
