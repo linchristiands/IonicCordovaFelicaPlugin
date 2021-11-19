6da60b67c4a19ac808ca7a5266808c153948687a
Christian Lin <linchristiands@gmail.com>
2021-11-19 16:50:09 +0900

    added ios ver

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NgZone } from '@angular/core';
-import { FelicaPlugin } from '@awesome-cordova-plugins/felica-plugin/ngx';
+//import { FelicaPlugin } from '@awesome-cordova-plugins/felica-plugin/ngx';
 
 declare var window: any;
 @Component({
@@ -13,7 +13,7 @@ export class HomePage {
   public pmm = '';
   public waonno = '';
   public enabled = true;
-  constructor(private zone: NgZone, private felica: FelicaPlugin) {}
+  constructor(private zone: NgZone) {}
 
   public async readNFC() {
     this.enabled = false;
@@ -37,8 +37,8 @@ export class HomePage {
       console.log(error);
       //alert(error);
     };
-    //window.cordova.plugins.FelicaPlugin.startNfc('', success, failure);
-    this.felica
+    window.cordova.plugins.FelicaPlugin.startNfc('', success, failure);
+    /* this.felica
       .startNfc('')
       .then((res) => {
         this.zone.run(() => {
@@ -49,18 +49,10 @@ export class HomePage {
       })
       .catch((e) => {
         //console.log(e);
-      });
+      }); */
     //let res = await this.felica.startNfc();
     //console.log(res);
   }
-  /* public async Hello() {
-    let hello = await this.felica.Hello("Testetststs");
-    let coolmethod = await this.felica.coolMethod();
-    let nativeToast = await this.felica.nativeToast();
-    console.log(hello);
-    console.log(coolmethod);
-    console.log(nativeToast);
-  } */
   public async stopNFC() {
     this.enabled = true;
     var success = function (message) {
@@ -71,7 +63,7 @@ export class HomePage {
       console.log(error);
       //alert(error);
     };
-    //window.cordova.plugins.FelicaPlugin.stopNfc('', success, failure);
-    await this.felica.stopNfc();
+    window.cordova.plugins.FelicaPlugin.stopNfc('', success, failure);
+    //await this.felica.stopNfc();
   }
 }
